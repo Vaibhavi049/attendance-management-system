2d2f4b901af618b86fc460030aa186295ff78794
Vaibhavi049 <agrawalva_2@rknec.edu>
2026-05-15 22:43:19 +0530

    Fixed duplicate endpoint mappings

--- src/main/java/com/attendance/attendance_management_system/controller/TestController.java
+++ src/main/java/com/attendance/attendance_management_system/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.attendance.attendance_management_system.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,15 +14,5 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "CI/CD Pipeline Deployment Successful!";
-    }
-
-    @GetMapping("/attendance/status")
-    public String status() {
-        return "Attendance Service is Running";
-    }
-
-    @PostMapping("/attendance/checkin")
-    public String checkin() {
-        return "Employee Checked In Successfully";
     }
 }
